a00417a9f1ab5e8b1594db9e71a872421e0b28a7
lex1112 <aleksei_fomichev@bk.ru>
2016-10-12 02:09:10 +0300

    commebt

---
+++ ExampleMapperOrm/PictureRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MapperOrm.Repository;
+
+namespace ExampleMapperOrm
+{
+    public class PictureRepository : Repository<Picture>
+    {
+        public PictureRepository()
+            : base("PhotoGallery")
+        {
+        }
+    }
+}
---
+++ ExampleMapperOrm/Profile.cs
@@ -0,0 +1,24 @@
+﻿using MapperOrm.Attributes;
+using MapperOrm.DBContext;
+using MapperOrm.Models;
+
+namespace ExampleMapperOrm
+{
+    [TableName("profile")]
+    public class Profile : EntityBase
+    {
+        [FieldName("profile_id")]
+        public override int Id { get; set; }
+
+        [FieldName("userinfo_id")]
+        public int? UserInfoId { get; set; }
+
+        [FieldName("role_id")]
+        //[ForeignKey("userinfo_id", "userinfo")]
+        public int RoleId { get; set; }
+
+        [RelatedEntity("Id", "UserProfileId", "PhotoGallery")]
+        public EntitySet<Picture> Pictures { get; set; }
+
+    }
+}
