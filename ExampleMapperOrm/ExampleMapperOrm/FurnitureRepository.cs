0f8844448d46c73b6ba0c04b8f0fb931a9b95bc8
lex1112 <aleksei_fomichev@bk.ru>
2016-10-12 01:11:39 +0300

    first commit

---
+++ ExampleMapperOrm/ExampleMapperOrm/FurnitureRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MapperOrm.Repository;
+
+namespace ExampleMapperOrm
+{
+    public class FurnitureRepository : Repository<Furniture>
+    {
+        public FurnitureRepository()
+            : base("FurnitureLocationDb")
+        {
+        }
+    }
+}
---
+++ ExampleMapperOrm/ExampleMapperOrm/Pictures.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MapperOrm.Attributes;
+using MapperOrm.DBContext;
+using MapperOrm.Models;
+
+namespace ExampleMapperOrm
+{
+
+
+    [TableName("user_picture")]
+    public class Picture : EntityBase
+    {
+        [FieldName("id")]
+        public override int Id { get; set; }
+
+        [FieldName("profile_id")]
+
+        public Int32? UserProfileId { get; set; }
+
+        [FieldName("src")]
+        public String Src { get; set; }
+
+        [FieldName("description")]
+        public String Description { get; set; }
+
+        [RelatedEntity("UserProfileId", "Id", "PhotoGallery")]
+        public EntitySet<Profile> Profile { get; set; }
+
+
+        //[NotMapped]
+        //public HttpPostedFileBase FileUpload { get; set; }
+
+    }
+}
